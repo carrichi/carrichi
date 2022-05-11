d972adfb3b1c6cb534a33761cf3520502ffcb209
carrichi <robertocarrichiunam@gmail.com>
2022-05-10 23:06:42 -0500

    Add media queries.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-
-fetch('../json/es/albums-es.json')
-	.then(response => response.json())
-	.then(data => console.log(data));
+fetch("../json/es/albums-es.json")
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].name);
+    }
+  });
